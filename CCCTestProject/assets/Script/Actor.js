78cf8867d54c600e72672b363b4cb6418bc0c112
admin <admin@G-iMac.local>
2016-06-15 18:13:09 +0800

    init_0615

---
+++ CCCTestProject/assets/Script/Actor.js
@@ -0,0 +1,25 @@
+var Unit = require("Unit");
+
+cc.Class({
+    extends: Unit,
+
+    properties: {
+        atk: 0,
+        def: 0,
+        hp: 0,
+        maxHP: 0,
+        movSd: 0,
+        atkSd: 0
+    },
+
+    // use this for initialization
+    onLoad: function () {
+
+    },
+
+    // called every frame, uncomment this function to activate update callback
+    // update: function (dt) {
+
+    // },
+
+});
